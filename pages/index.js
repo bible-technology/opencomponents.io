310f481a9ef5bf2cc0e6e5e2e0a3336e73699984
Jesse Griffin <jag3773@gmail.com>
2021-07-22 11:02:55 -0400

    Fixed site title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Open Components</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
