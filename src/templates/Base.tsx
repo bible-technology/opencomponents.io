41d46cf6916ec48f996c3ace07efdb81d8ac25de
Latryll <latryll.alimes@citylightcs>
2021-11-12 01:46:14 +0800

    added another component for youtube player

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,12 +1,12 @@
-import { Meta } from '../layout/Meta';
-import { AppConfig } from '../utils/AppConfig';
-import { FrontLanding } from './FrontLanding';
+import { Meta } from '../layout/Meta'
+import { AppConfig } from '../utils/AppConfig'
+import { FrontLanding } from './FrontLanding'
 
 const Base = () => (
-  <div className="antialiased text-gray-600">
-    <Meta title={AppConfig.title} description={AppConfig.description} />
-    <FrontLanding />
-  </div>
-);
+    <div className="antialiased text-gray-600">
+        <Meta title={AppConfig.title} description={AppConfig.description} />
+        <FrontLanding />
+    </div>
+)
 
-export { Base };
+export { Base }
