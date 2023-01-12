fc02544a584ebf8603bf3f3520e6fe7471553cd1
Vitek Foxprogs <foxprogs@gmail.com>
2023-01-12 17:36:00 +0400

    change partners and add summary

--- components/Partners.js
+++ components/Partners.js
@@ -7,11 +7,11 @@ import ip_logo from '../public/logo/idiomaspuentes-logo.svg'
 import eten_logo from '../public/logo/eten_logo.png'
 
 const orgs = [
-  { logo: uw_logo, url: 'https://unfoldingword.org', w: 228 },
+  { logo: eten_logo, url: 'https://eten.bible/', w: 131 },
   { logo: tt_logo, url: 'https://texttree.org', w: 145 },
-  { logo: ip_logo, url: '', w: 208 },
-  { logo: bc_logo, url: '', w: 112 },
-  { logo: eten_logo, url: '', w: 131 },
+  { logo: ip_logo, url: 'https://idiomaspuentes.org/', w: 208 },
+  { logo: uw_logo, url: 'https://unfoldingword.org', w: 228 },
+  { logo: bc_logo, url: 'https://bridgeconn.com/', w: 112 },
 ]
 function Partners() {
   return (
@@ -21,10 +21,7 @@ function Partners() {
       </div>
       <div className="flex-wrap flex mt-6">
         {orgs.map((org) => (
-          <div
-            className="grayscale brightness-50 opacity-70 hover:opacity-100 hover:grayscale-0 hover:brightness-100 transition-all m-4"
-            key={org.logo}
-          >
+          <div className="m-4" key={org.logo}>
             <a target="_blank" href={org.url} className="cursor-pointer" rel="noreferrer">
               <Image src={org.logo} alt={org.url} width={org.w} height="40" />
             </a>
