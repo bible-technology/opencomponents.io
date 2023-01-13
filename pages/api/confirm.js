3829b8044e72bdf6357d7586cd556d359ae40031
Vitek Foxprogs <foxprogs@gmail.com>
2023-01-13 11:46:55 +0400

    new calendar link

--- pages/api/confirm.js
+++ pages/api/confirm.js
@@ -22,7 +22,7 @@ Thank you for signing up for the Open Components Hackathon 2023.<br/>
 <b>Learnathon</b> (Online): <b>February&nbsp;13&nbsp;-&nbsp;17,2023</b><br/>
 <b>Hackathon</b> (Online/Multi-site): <b>February&nbsp;20&nbsp;-&nbsp;24,2023</b></p>
 <p>If you have registered as an active participant, please ensure you are part of a team. If you'd like to create a new team, please fill out a <a href="https://forms.gle/VbeTG472AKbhpLnw9">form</a>. If you'd like to join an existing team, please reach out to other participants on <a href="https://discord.gg/jtTRhEMH4q">Discord</a>.</p>
-<p>Please <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmNoNTNoa2RnNWNnbHFvdHQ3c2NucjVydjlfMjAyMzAyMTNUMTQwMDAwWiBiaW5jeS5qb2JieUB1bmZvbGRpbmd3b3JkLm9yZw&tmsrc=bincy.jobby%40unfoldingword.org&scp=ALL">click here</a> to add this event to your calendar. You can visit our <a href="https://opencomponents.io/hackathon/schedule">hackathon site</a> to learn more about the event. We look forward to seeing you soon!
+<p>Please <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmNoNTNoa2RnNWNnbHFvdHQ3c2NucjVydjlfMjAyMzAyMTNUMTQwMDAwWiBjX2VhY2RmYzkxY2E1ZjFmOWZmODAzZWNkZTliNzMzZDBjODU0ZjU4YjQ3MWNhOThkM2FkOGMxMDlkNjExMzgyY2JAZw&tmsrc=c_eacdfc91ca5f1f9ff803ecde9b733d0c854f58b471ca98d3ad8c109d611382cb%40group.calendar.google.com&scp=ALL">click here</a> to add this event to your calendar. You can visit our <a href="https://opencomponents.io/hackathon/schedule">hackathon site</a> to learn more about the event. We look forward to seeing you soon!
 </p>
 <p>Thanks,<br/>
 Chris Klapp, aka klappy<br/>
